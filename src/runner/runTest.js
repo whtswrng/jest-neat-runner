668c175fcdf36de38086b4e1d6d3ab6970d46dc3
tomas-gold_swi <tomas.gold@solarwinds.com>
2024-10-12 13:35:08 +0200

    wip

--- src/runner/runTest.js
+++ src/runner/runTest.js
@@ -1,26 +1,9 @@
 const _runTest = require("./_runTest").default;
-const path = require("path");
 
 let successTests = 0;
 let failedTests = 0;
 
-const memoryLimitInGb = 0.1;
-const cpuLimitInPerc = 10;
-
 async function runTest(...args) {
-  // const memoryUsage = getMemoryUsage();
-  // const memoryPerc = memoryUsage.memoryUsagePercentage;
-  // const memoryLeftGb = memoryUsage.totalMemory - memoryUsage.usedMemory;
-  // const cpuLoadPerc = getCpuLoad();
-
-  // if(100-cpuLoadPerc <= cpuLimitInPerc || memoryLeftGb <= memoryLimitInGb) {
-  //   await waitFor(20);
-  //   return runTest(...args);
-  // }
-
-  // console.log("Memory Left:", memoryLeftGb);
-  // console.log("Overall CPU Load:", getCpuLoad(), "%");
-
   const testFile = args[0];
   const config = args[2];
   const globals = config.globals;
@@ -52,51 +35,6 @@ async function runTest(...args) {
     global._NEAT_REMOVE_CACHE = testFile;
     return _runTest(...args);
   }
-}
-
-async function waitFor(ms) {
-  return new Promise((res) => setTimeout(res, ms));
-}
-
-const os = require("os");
-
-// Function to get overall CPU load
-function getCpuLoad() {
-  const cpus = os.cpus();
-
-  let userTime = 0;
-  let niceTime = 0;
-  let sysTime = 0;
-  let idleTime = 0;
-  let irqTime = 0;
-
-  cpus.forEach((cpu) => {
-    userTime += cpu.times.user;
-    niceTime += cpu.times.nice;
-    sysTime += cpu.times.sys;
-    idleTime += cpu.times.idle;
-    irqTime += cpu.times.irq;
-  });
-
-  const totalTime = userTime + niceTime + sysTime + idleTime + irqTime;
-  const loadTime = totalTime - idleTime;
-
-  const loadPercentage = (loadTime / totalTime) * 100;
-  return loadPercentage.toFixed(2);
-}
-
-function getMemoryUsage() {
-  const totalMemory = os.totalmem(); // Total system memory in bytes
-  const freeMemory = os.freemem(); // Free system memory in bytes
-
-  const usedMemory = totalMemory - freeMemory; // Used memory in bytes
-  const memoryUsagePercentage = (usedMemory / totalMemory) * 100;
-
-  return {
-    totalMemory: (totalMemory / (1024 * 1024 * 1024)).toFixed(2), // Convert to GB
-    usedMemory: (usedMemory / (1024 * 1024 * 1024)).toFixed(2), // Convert to GB
-    memoryUsagePercentage: memoryUsagePercentage.toFixed(2), // Memory usage percentage
-  };
 }
 
 module.exports = runTest;
